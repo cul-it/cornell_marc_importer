aa3424b01e176e7880d0d3a5c327ef7e0416e803
starsplatter <jrc88@cornell.edu>
2018-05-04 14:43:07 -0400

    first commit. map bibid.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cornell MARCXML Importer
+
+Building up some custom mappings for importing bib records. 
+
+## Installing it
+
+To install, just activate the plugin in your config/config.rb file by
+including an entry such as:
+
+     # If you have other plugins loaded, just add 'unlv_importer' to
+     # the list
+     AppConfig[:plugins] = ['local', 'other_plugins', 'cornell_marcxml_importer']
+	
---
+++ backend/model/cornell_marcxml_converter.rb
@@ -0,0 +1,40 @@
+
+class CornellMarcXMLConverter < MarcXMLConverter
+	
+	# Create the import type for marcxml agents 
+	def self.import_types(show_hidden = false)
+		[
+			{
+				:name => "MarcXML (Cornell)",
+				:description => "Import MARC XML records for Cornell"
+			}
+		]
+	end
+	
+	# Alter the information recieved from the resource 
+	def initialize(input_file)
+		super(input_file)
+	end
+		
+	def self.instance_for(type, input_file)
+		if type == "MarcXML (Cornell)"
+			self.new(input_file)
+		else
+			nil
+		end
+	end
+	
+end
+
+CornellMarcXMLConverter.configure do |config|
+
+  config.doc_frag_nodes.uniq! 
+  config["/record"][:obj] = :resource
+  config["/record"][:map]["controlfield[@tag='001']"] = -> resource, node {
+    bibid = node.inner_text
+      resource.user_defined ||= ASpaceImport::JSONModel(:user_defined).new
+      unless resource.user_defined['real_1']
+        resource.user_defined['real_1'] = bibid
+      end
+  }
+end
