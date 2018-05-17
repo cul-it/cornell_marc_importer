f2644b816221384000bca9a303ba2bc1464ce981
starsplatter <jrc88@cornell.edu>
2018-05-17 14:26:15 -0400

    don't ignore our wacky 520s

--- backend/model/cornell_marcxml_basemap_patch.rb
+++ backend/model/cornell_marcxml_basemap_patch.rb
@@ -18,6 +18,24 @@ def cornell_citation_note(note_type, label = nil, template=nil, *tmpl_args)
     }
   end
 
+  def cornell_520(note_type, label = nil, template=nil, *tmpl_args)
+    {
+      :obj => :note_multipart,
+      :rel => :notes,
+      :map => {
+        "self::datafield" => -> note, node {
+          content =  node.inner_text
+
+          label = label.call(node) if label.is_a?(Proc)
+
+          note.send('label=', label) if label
+          note.type = note_type
+          note.subnotes = [{'jsonmodel_type' => 'note_text', 'content' => content}]
+        }
+      }
+    }
+  end
+
 
   def cornell_related_materials_note(note_type, label = nil, template=nil, *tmpl_args)
     {
